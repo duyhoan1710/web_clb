47ba384dccb803d15434ff4282d0cf5fe963e372
duy_hoan <hoanduy1710@gmail.com>
2020-04-18 08:46:46 +0700

    add cors

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ let morgan = require('morgan');
 let logger = require('./logger/logger');
 
 let app = express();
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use(morgan(':method :url :status ', {"stream": logger.stream}));
