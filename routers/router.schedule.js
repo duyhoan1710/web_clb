4af5081c3049d8fa46b276f235b7c3ad93a2eae2
hoan.nguyenduy <hoan.nguyenduy@amela.vn>
2021-01-24 18:12:42 +0700

    .

--- routers/router.schedule.js
+++ routers/router.schedule.js
@@ -1,5 +1,5 @@
 let controllerSchedule = require('../controllers/controller.schedule');
-let authorize = require('../middleware/authenticate/isAuth');
+// let authorize = require('../middleware/authenticate/isAuth');
 module.exports = (app)=>{
     // app.get('/api/schedule' ,authorize.isAuth, controllerSchedule.get);
     // app.put('/api/schedule' ,authorize.isAuth, controllerSchedule.update);
